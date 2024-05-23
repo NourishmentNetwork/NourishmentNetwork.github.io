53019bd592bd2fd293a19e18a7ad36af16d6a0d7
Magic Man <65839659+Magician357@users.noreply.github.com>
2024-05-23 12:12:57 -0700

    Save the csv to the filesystem

---
+++ download_csv.py
@@ -0,0 +1,26 @@
+# import the csv module
+import pandas as pd
+# import numpy as np
+
+# link to the data
+url="https://docs.google.com/spreadsheets/d/1o3FfjUsO-oksVLs92WX721G2GCswa2Qdu8WD3qZ6BGM/export?format=csv"
+
+# load in the data
+c = pd.read_csv(url)
+print(c)
+
+# use negative indexing to remove last row (average)
+c = c[:-1]
+print(c)
+
+# same thing, but remove last two columns (unececary data)
+c = c.iloc[:, :-2]
+print(c)
+
+# Filter out all rows with no data entered for the banana price
+# Easy way to remove all rows with no data
+c = c[c["Banana Price (lb)"].notnull()]
+print(c)
+
+# Save to csv
+c.to_csv("prices.csv")
