f85663a6841ac558d546f54df0fa8f2c4d44ee5d
Magic Man <65839659+Magician357@users.noreply.github.com>
2024-05-23 19:21:59 -0700

    add more comments

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,11 @@
 // get data from url
 async function getData(url){
+    // creates a fetch request and then returns just the response
     const curRequest = new Request(url);
     return fetch(curRequest)
-        .then((response)=>response.text())
+        .then((response)=>response.text()) // get only the text
         .then((text) => {
-            return text
+            return text // return it
         });
 }
 
@@ -13,18 +14,24 @@ const tableElement = document.getElementById("table");
 
 // load and return the csv as a json object
 async function get_csv(){
-    return await getData("/prices.csv").then((csv) => {return Papa.parse(csv,{"header":true})["data"]});
+    // first use the get data function to open the csv file and get it as a string
+    return await getData("/prices.csv")
+        .then((csv) => { // then use papaparse to change it to a json object
+            return Papa.parse(csv,{"header":true})["data"] 
+            // also only return the data, and not the other things papaparse returns
+            // e.g. errors
+        });
 }
 
-var prices_json;
+var prices_json; // create global variable
 
 async function load_table(){
     // load the json into the global variable
     prices_json = await get_csv();
 
-    // add each row
+    // loop over json then add each row
     prices_json.forEach(row => {
-        // let current_row = document.createElement("div");
+        // use an html template, and fill in the costs and other data
         let current_html = `<div class="row" data-full="${btoa(JSON.stringify(row))}"><h3>${row["Store Name"]}</h3> 
 <div class="rowdata">
 <p class="priceelement">Bananas: $${row["Banana Price (lb)"]} per pound</p>
@@ -35,7 +42,7 @@ async function load_table(){
 <p class="priceelement">Tomatoes: $${row["Tomato Price (lb)"]} per pound</p>
 </div></div>`
         // current_row.innerHTML=current_html
-        tableElement.innerHTML+=current_html
+        tableElement.innerHTML+=current_html // append the element to the table
     });
 
     // synchronize scrolling for each div
@@ -47,29 +54,32 @@ async function load_table(){
     }) );
 }
 
-load_table();
+load_table(); // run the function
 
+// function that hides and shows rows based on what the user inputs
 function filter_rows(){
 
+    // load in the text box objects
     const price_text_boxes = document.querySelectorAll(".pricetextbox");
 
+    // get the store name the user inputted 
     let store_name = document.getElementById("storenamesearch").value.toLowerCase();
-    // console.log(store_name);
+    // changed to lowercase so it can match better
 
     // take every text box and load it into a list
     let prices = [];
     price_text_boxes.forEach(text_box =>{
-        if (text_box.value != ""){
-            let result = parseFloat(text_box.value);
-            if (isNaN(result)){
+        if (text_box.value != ""){ // skip over empty boxes
+            let result = parseFloat(text_box.value); // attempt to convert into a float
+            if (isNaN(result)){ // if the box has invalid inputs
                 alert("One or more texboxes are invalid!");
-                prices.push(1000.0)
+                prices.push(1000.0) // no price should ever be over 1000, so it is used as a placeholder
             } else {prices.push(result);}
-        } else {prices.push(1000.0)} // no price should ever be over 1000, so it is used as a placeholder
+        } else {prices.push(1000.0)} // same as other 1000
     })
 
 
-    console.log(prices);
+    console.log(prices); // log the prices for debug
     
     // get all rows except the searchbar row
     const divs = document.querySelectorAll('.row:not(.search)');
@@ -78,17 +88,19 @@ function filter_rows(){
         let row_data = JSON.parse(atob(row.dataset.full));
         
         // filter out the costs
-        let invalid = (parseFloat(row_data["Banana Price (lb)"]) > prices[0]) || 
+        let invalid = (parseFloat(row_data["Banana Price (lb)"]) > prices[0]) ||  // check each price against what the user entered
                         (parseFloat(row_data["Strawberry Price (oz)"]) > prices[1]) ||
                         (parseFloat(row_data["Apple Price (oz)"]) > prices[2]) ||
                         (parseFloat(row_data["Potato Price (lb)"]) > prices[3]) ||
                         (parseFloat(row_data["Onion Price (lb)"]) > prices[4]) ||
                         (parseFloat(row_data["Tomato Price (lb)"]) > prices[5]) ||
-                        !(row_data["Store Name"].toLowerCase().includes(store_name));
+                        !(row_data["Store Name"].toLowerCase().includes(store_name)); // if the store's name contains what the user entered
 
+        // hide any rows that don't match the search
         if (invalid){
             row.classList.add("hidden");
         } else {
+            // unhide rows that do match
             row.classList.remove("hidden");
         }
     })
